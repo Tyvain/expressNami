72c2934173ad0a4c5d5cf313bde1aeb3d38efa2a
Tyvain <tyvain@gmail.com>
2016-11-30 11:00:03 +1100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Send mail */
+router.post('/postmail', function (req, res) {
+    console.log(req.body.title);
+    console.log(req.body.description);
+});
+
 module.exports = router;
